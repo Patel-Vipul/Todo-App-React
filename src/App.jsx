b929485c568ac70850eca2999d9d731d280ee341
Patel Vipul <patelvipul9601@gmail.com>
2025-11-06 01:25:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ useEffect(() => {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <div className="flex">
-            <h1 className="text-2xl font-bold text-center mb-8 mt-2 w-full  ">
+            <h1 className="text-2xl font-extrabold text-center mb-8 mt-2 w-full  ">
               Manage Your Todos
             </h1>
           </div>
